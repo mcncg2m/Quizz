f9e8a9cc7faf52a47446bb02d464a32abe2d7223
fjcamposg <fj.camposg@gmail.com>
2015-08-06 20:04:20 +0200

    Publicacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.param('quizId', quizController.load);
-
+router.param('commentId', commentController.load);
 router.get('/login', sessionController.new);
 router.post('/login', sessionController.create);
 router.get('/logout', sessionController.destroy);
@@ -29,5 +29,6 @@ router.get('/author', authorController.author);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
-
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+sessionController.loginRequired,commentController.publish);
 module.exports = router;
